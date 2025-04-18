2768cdd21fe102d5fd9c0cc0acdc1e806a9cae24
Oscar Debeuret <debeuret.oscar@gmail.com>
2025-04-18 16:31:01 +0200

    gitgnore fix

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/loginn', name: 'app_loginn')]
     public function index(): Response
     {
         $is_register = false;
