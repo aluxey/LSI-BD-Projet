390d639ad74634c7ce73f4f1c3a11497d63470aa
gibos <geoffrey.ibos@etu.u-bordeaux.fr>
2025-05-01 13:28:03 +0200

    Ajout des traitements back pour l'authent

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,21 +6,63 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\Membre;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // get the login error if there is one
+    public function login(
+        Request $request,
+        AuthenticationUtils $authenticationUtils,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        Security $security
+    ): Response {
+        // Partie LOGIN
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', [
+    
+        // Partie REGISTER
+        $user = new Membre();
+        $registerForm = $this->createForm(RegistrationFormType::class, $user);
+        $registerForm->handleRequest($request);
+    
+        $registerError = null;
+        $isRegister = false;
+    
+        if ($registerForm->isSubmitted()) {
+            $isRegister = true; // active le bon onglet
+    
+            if ($registerForm->isValid()) {
+                $plainPassword = $registerForm->get('plainPassword')->getData();
+    
+                $user->setPassword(
+                    $passwordHasher->hashPassword($user, $plainPassword)
+                );
+    
+                $entityManager->persist($user);
+                $entityManager->flush();
+    
+                // Connexion automatique (optionnel)
+                $security->login($user);
+    
+                return $this->redirectToRoute('app_home');
+            } else {
+                $registerError = 'Please correct the errors in the form.';
+            }
+        }
+    
+        return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+            'is_register' => $isRegister,
+            'register_form' => $registerForm->createView(),
+            'register_error' => $registerError,
         ]);
     }
 
